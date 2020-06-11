0471a8a2572c365a05793ff6ca1848e74b7a2618
TobyGroves <Toby.tobtob@gmail.com>
2020-06-11 16:24:01 +0100

    forgot to call the function int the api like a doof

--- mpu6050RESTAPINOPRO.py
+++ mpu6050RESTAPINOPRO.py
@@ -206,6 +206,7 @@ app = FlaskAPI(__name__)
 
 @app.route('/calibrate',methods=["GET"])
 def api_calibrate():
+    calibration()
     return {
         "status" : "Calibration Complete",
         "mean_ax" : mpu1.mean_ax,
